3e96fdd377688ed52e70e873fec1dabcc9f1d5ba
Mia5391 <mariakrasteva5391@gmail.com>
2018-07-31 09:25:23 -0400

    emailJS installed

--- Admin/assets/adminapp.js
+++ Admin/assets/adminapp.js
@@ -4,17 +4,16 @@ window.onload=()=>{
 showAllVisitors = ()=>{
       table.innerHTML =''
       getAllVisitors().on('child_added', (visitor) => {
-        console.log(visitor);
-
-              visitor = visitor.val()
+        getVisitorData(visitorId.val().id).then(visitor => {
+            visitor = visitor.val()
               table.innerHTML += `
                 <tr>
                     <td>${visitor.name}</td>
                     <td>${visitor.rut}</td>
+                    <td>${visitor.email}</td>
                     <td>${visitor.destination}</td>
                     <td>${visitor.subject}</td>
                     <td>${visitor.checkingTime}</td>
-                    <td>${visitor.email}</td>
                     <td>  <i class="material-icons right" onclick="checkout('${visitor.id}')">keyboard_arrow_right
                     </i></td>
                 </tr>`
--- User/assets/sendMail.js
+++ User/assets/sendMail.js
@@ -1,25 +1,16 @@
-// using SendGrid's v3 Node.js Library
-// https://github.com/sendgrid/sendgrid-nodejs
+window.onload = function() {
+            document.getElementById('contact-form').addEventListener('file', function(event) {
+                event.preventDefault();
+                this.contact_number.value = Math.random() * 100000 | 0;
+                emailjs.sendForm('gmail', 'new_visitor', this);
+            });
 
-function sendMail(){
-  const recipientEmail = 'ifsclreception@gmail.com';
-  const visitorName = 'Maya';
+            
 
-  const sender = 'reception@if.com';
-  const emailSubject = 'You have a visitor';
-  const emailBody = visitorName + ' is coming to visit you.';
-
-
-var templateParams = {
-    name: 'James',
-    notes: 'Check this out!'
-};
-
-emailjs.send('<YOUR SERVICE ID>', '<YOUR TEMPLATE ID>', templateParams)
+emailjs.send('<gmail>', '<new_visitor>', templateParams)
     .then(function(response) {
        console.log('SUCCESS!', response.status, response.text);
     }, function(error) {
        console.log('FAILED...', error);
     });
-});
 };
