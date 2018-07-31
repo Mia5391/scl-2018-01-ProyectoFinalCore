0811effe6870ede284d1798b39e259b907374128
AriellaRogueN <maitilydevidasi@gmail.com>
2018-07-31 18:06:10 -0400

    cambios en estructura de admin

--- public/Admin/assets/main.js
+++ public/Admin/assets/main.js
@@ -14,18 +14,20 @@ showAllVisitors = () => {
                     <img src="${visitor.photo}">
                   </div>
                   <div class="col s4">
-                    <p>${visitor.name}</p>
+                    <p><strong>${visitor.name}</strong></p>
                     <p>${visitor.email}</p>
                      <p>${visitor.rut}</p>
                   </div>
                   <div class="col s3">
-                    <p>${visitor.destination}</p>
+                    <p><strong>Destino</strong></p>
+                    <p class="visitor-destino">${visitor.destination}</p>
                      <p>${visitor.subject}</p>
                   </div>
 
                   <div class="col s3">
-                    <p><span>${visitor.checkingTime}<span></p>
-                    <p><span>${visitor.checkoutTime}<span></p>
+                   <p><strong>Hora</strong></p>
+                    <p>In <span class="visitor-time">${visitor.checkingTime}<span></p>
+                    <p>Out <span class="visitor-time">${visitor.checkoutTime}<span></p>
                   </div>
                 </div>`
                 
@@ -45,26 +47,27 @@ searchVisitorOrganizacion = (organizacion) => {
       visitors.forEach(visitor => {
         visitor = visitor[1]
         visitorList.innerHTML += `
-               <div class="row visitor">
+              <div class="row visitor">
                    <div class="col s2">
                     <img src="${visitor.photo}">
                   </div>
                   <div class="col s4">
-                    <p>${visitor.name}</p>
+                    <p><strong>${visitor.name}</strong></p>
                     <p>${visitor.email}</p>
                      <p>${visitor.rut}</p>
                   </div>
                   <div class="col s3">
-                    <p>${visitor.destination}</p>
+                    <p><strong>Destino</strong></p>
+                    <p class="visitor-destino">${visitor.destination}</p>
                      <p>${visitor.subject}</p>
                   </div>
 
                   <div class="col s3">
-                    <p><span>${visitor.checkingTime}<span></p>
-                    <p><span>${visitor.checkoutTime}<span></p>
+                   <p><strong>Hora</strong></p>
+                    <p>In <span class="visitor-time">${visitor.checkingTime}<span></p>
+                    <p>Out <span class="visitor-time">${visitor.checkoutTime}<span></p>
                   </div>
                 </div>`
-
       });
         
     })
